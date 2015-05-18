8123693f4bd3188632e5f3fd714345ac3f6e84a6
goungy <jdubois69@gmail.com>
2015-05-18 09:07:10 +0200

    Initializing logger and instantiating WordStatistics.

--- CROISADE/src/main.py
+++ CROISADE/src/main.py
@@ -1,14 +1,19 @@
 import sys
 
 from core.Lexique import Lexique
+from core.statistics import WordStatistics
+from controller.Logger import Logger
 
+Logger.initialize_logger(loglevel = Logger.NORMAL)
 
-lex_file = '/home/jerome/projets/croisade/CROISADE/data/lexiques/ultimate_556105.txt'
-word_length = 3
+lex_file = '../data/lexiques/ultimate_556105.txt'
+word_length = 10
 
 if len(sys.argv) == 2:
     word_length = sys.argv[1]
 
 l = Lexique(lex_file, word_length)
+word_statistics = WordStatistics(l)
+
 
 
