bc6e9fee01dafb9e222eb28ba1a75dd160f3522b
goungy <jdubois69@gmail.com>
2015-05-19 11:34:57 +0200

    Add method to get index of word in lexique

--- CROISADE/src/core/Lexique.py
+++ CROISADE/src/core/Lexique.py
@@ -9,3 +9,6 @@ class Lexique(object):
 		with open(filepath) as f:
 			self.words = [ w.strip('\n') for w in f if len(w.strip('\n')) == self.word_length ]
 		Logger.log(str(len(self.words)) + " words of length " + str(self.word_length) + " loaded from " + self.filepath, self) 
+		
+	def get_index_of_word(self, word):
+		return self.words.index(word)
